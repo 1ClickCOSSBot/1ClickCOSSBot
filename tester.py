876e0578618d5b9da6ad486c02cf32dfff8c4d0b
1ClickCOSSBot <58043792+1ClickCOSSBot@users.noreply.github.com>
2019-12-02 18:51:03 -0500

    Testing Python API wrapper
    Created a test file to start testing interaction of Bot UI with python API wrapper for COSS exchange.

--- tester.py
+++ tester.py
@@ -1,12 +1,7 @@
-from glob import glob
+from pycoss import PyCOSSClient
 
-print("This is a test file")
+coss_client = PyCOSSClient(api_public=publicKey,
+		                           api_secret=privateKey)
 
-if glob("history/*__history.txt"):
-	print("This file exists")
-
-existingFiles = glob("history/*__history.txt")
-
-file1 = existingFiles[0]
-
-print(file1.split("\\")[1])
+coss_ob = coss_client.get_order_book(symbol="COS_ETH")
+print("Price: " + coss_ob["bids"][1][0] + " Amount: " + coss_ob["bids"][0][1])
